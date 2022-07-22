a5c2b7530ca4f94ec97210e8fdd1bc9be3169e8f
zhuzhenfeng.code <zhuzhenfeng1993@gmail.com>
2022-07-22 23:12:38 +0800

    finished

--- pages/index.js
+++ pages/index.js
@@ -2,17 +2,81 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Nav from '../components/Nav.jsx';
 import { useState } from 'react';
+import * as fcl from '@onflow/fcl'
+import { useEffect } from 'react'
 
 export default function Home() {
   const [newGreeting, setNewGreeting] = useState('');
+  const [greeting, setGreeting] = useState('');
+  const [txStatus, setTxStatus] = useState('Run Transaction');
 
-  function printHello() {
-    console.log("Hello there! Jacob is soooooo much cooler than me.")
+
+  async function runTransaction() {
+    setTxStatus('Requesting...')
+    console.log("Running Transaction!")
+    console.log("Changing the greeting to: " + newGreeting);
+
+    const txId = await fcl.mutate({
+      cadence: `
+      import HelloWorld from 0xd55b625258390fec
+      transaction(myNewGreeting: String) {
+
+        prepare(signer: AuthAccount) {}
+  
+        execute {
+          HelloWorld.changeGreeting(newGreeting: myNewGreeting)
+        }
+      }
+      `,
+      args: (arg, t) => [
+        arg(newGreeting, t.String)
+      ],
+      proposer: fcl.authz,
+      payer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 999,
+    })
+
+    console.log('Here is the transaction: ' + txId)
+    fcl.tx(txId).subscribe(res => {
+      console.log(res)
+      if (res.status === 0 || res.status === 1) {
+        setTxStatus('Pending...');
+      } else if (res.status === 2) {
+        setTxStatus('Finalized...')
+      } else if (res.status === 3) {
+        setTxStatus('Executed...');
+      } else if (res.status === 4) {
+        setTxStatus('Sealed!');
+
+        setTimeout(() => setTxStatus('Run Transaction'), 2000); // We added this line
+      }
+    })
+
+    await fcl.tx(txId).onceSealed()
+    executeScript()
   }
 
-  function printGoodbye() {
-    console.log("Goodbye.")
+  async function executeScript() {
+    const resp = await fcl.query({
+      cadence: `
+      import HelloWorld from 0xd55b625258390fec
+
+      pub fun main(): String {
+          return HelloWorld.greeting
+      }
+      `,
+      args: (arg, t) => []
+    })
+
+    setGreeting(resp)
+
+    console.log("Response from our script: " + resp)
   }
+
+  useEffect(() => {
+    executeScript()
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -24,16 +88,18 @@ export default function Home() {
 
       <Nav />
 
-      <main className={styles.main}>
+      <div className={styles.welcome}>
         <h1 className={styles.title}>
           Welcome to my <a href="https://academy.ecdao.org" target="_blank">Emerald DApp!</a>
         </h1>
-        <p>This is a DApp created by Jacob Tucker.</p>
+        <p>This is a DApp created by Fenix.</p>
+      </div>
 
+      <main className={styles.main}>
+        <p>{greeting}</p>
         <div className={styles.flex}>
-          <button onClick={printHello}>Hello</button>
           <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" />
- 
+          <button onClick={runTransaction} disabled={txStatus != 'Run Transaction'}>{txStatus}</button>
         </div>
       </main>
     </div>
