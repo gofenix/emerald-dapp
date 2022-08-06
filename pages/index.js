500bf4cce16c24497aa201dfb42d019f52d02adf
zhuzhenfeng.code <zhuzhenfeng.code@bytedance.com>
2022-08-06 22:15:58 +0800

    add mint demo
    Change-Id: I69dd77a94c8ac3e19aa6d4d6dd308f3359ceafb6

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,8 @@ export default function Home() {
   const [greeting, setGreeting] = useState('');
   const [txStatus, setTxStatus] = useState('Run Transaction');
 
+  const [mintStatus, setMintStatus] = useState('Mint Race Number Token')
+
 
   async function runTransaction() {
     setTxStatus('Requesting...')
@@ -57,6 +59,83 @@ export default function Home() {
     executeScript()
   }
 
+  async function mintRaceNumber() {
+    setMintStatus('Requesting...')
+    console.log("Running Transaction!")
+
+    const txId = await fcl.mutate({
+      cadence: `
+      // import RaceNumberContract from 0xf8d6e0586b0a20c7
+
+      // testnet
+      import RaceNumberContract from 0x16fa33cab0a7b7c2
+      
+      transaction {
+          let receiverRef: &{RaceNumberContract.NFTReceiver}
+      
+          let minterRef: &RaceNumberContract.NFTMinter
+      
+          let metadata: {String: String}
+      
+          prepare(account: AuthAccount) {
+              if account.borrow<&RaceNumberContract.NFTCollection>(from: /storage/NFTCollectionRaceNumber) == nil {
+                  account.save(<-RaceNumberContract.createNFTCollection(), to: /storage/NFTCollectionRaceNumber)
+                  account.link<&{RaceNumberContract.NFTReceiver}>(/public/NFTReceiverRaceNumber, target: /storage/NFTCollectionRaceNumber)
+              }
+      
+              if account.borrow<&RaceNumberContract.NFTMinter>(from: /storage/NFTMinterRaceNumber) == nil{
+                  account.save(<-RaceNumberContract.createNFTMinter(), to: /storage/NFTMinterRaceNumber)
+              }
+       
+              self.receiverRef = account.getCapability<&{RaceNumberContract.NFTReceiver}>(/public/NFTReceiverRaceNumber)
+                  .borrow()
+                  ?? panic("Could not borrow receiver reference")
+      
+              self.minterRef = account.borrow<&RaceNumberContract.NFTMinter>(from: /storage/NFTMinterRaceNumber)
+                  ?? panic("Could not borrow minter reference")
+      
+              self.metadata = {
+              "number": "001",
+              "gameType": "1",
+              "templateType": "1",
+              "imgUrl": "abcdefg"
+              }
+          }
+      
+          execute {
+              let newToken <- self.minterRef.mint(self.metadata)
+              self.receiverRef.deposit(token: <-newToken)
+          }
+      }
+      `,
+      args: (arg, t) => [
+        // arg(newGreeting, t.String)
+      ],
+      proposer: fcl.authz,
+      payer: fcl.authz,
+      authorizations: [fcl.authz],
+      limit: 999,
+    })
+
+    console.log('Here is the transaction: ' + txId)
+    fcl.tx(txId).subscribe(res => {
+      console.log(res)
+      if (res.status === 0 || res.status === 1) {
+        setMintStatus('Pending...');
+      } else if (res.status === 2) {
+        setMintStatus('Finalized...')
+      } else if (res.status === 3) {
+        setMintStatus('Executed...');
+      } else if (res.status === 4) {
+        setMintStatus('Sealed!');
+
+        setTimeout(() => setMintStatus('Mint Race Number Token'), 2000); // We added this line
+      }
+    })
+
+    await fcl.tx(txId).onceSealed()
+  }
+
   async function executeScript() {
     const resp = await fcl.query({
       cadence: `
@@ -100,7 +179,10 @@ export default function Home() {
         <div className={styles.flex}>
           <input onChange={(e) => setNewGreeting(e.target.value)} placeholder="Hello, Idiots!" />
           <button onClick={runTransaction} disabled={txStatus != 'Run Transaction'}>{txStatus}</button>
+          <button onClick={mintRaceNumber} disabled={mintStatus != 'Mint Race Number Token'}>{mintStatus}</button>
+
         </div>
+
       </main>
     </div>
   )
